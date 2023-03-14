dfd67f8696d9d192f320dc0658bbd2cbf3b8df4d
getsetgo1 <dnwls1313@gmail.com>
2023-03-14 15:05:26 +0900

    todo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell
+	} from 'flowbite-svelte';
+	let todo = [
+		{
+			content: 'study',
+			status: 0
+		},
+		{
+			content: 'dinner',
+			status: 1
+		}
+	];
+</script>
+
+<Table>
+	<TableHead>
+		<TableHeadCell>할 일 </TableHeadCell>
+		<TableHeadCell>action</TableHeadCell>
+	</TableHead>
+	<TableBody class="divide-y">
+        {#each todo as item}
+            <TableBodyRow>
+                <TableBodyCell>{item.content}</TableBodyCell>
+                <TableBodyCell>{item.status}</TableBodyCell>
+            </TableBodyRow>
+        {/each}
+	</TableBody>
+</Table>
---
+++ src/routes/todo/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+	let todo = [
+		{
+			content: 'study',
+			status: 0
+		},
+		{
+			content: 'dinner',
+			status: 1
+		}
+	];
+	let value = '';
+
+	function handler() {
+		todo = [...todo, { content: value, status: 0 }];
+		value = '';
+	}
+	//해당 아이템 완성 버튼 클릭
+	// @ts-ignore
+	function complete(index) {
+		todo[index].status = 1;
+	}
+
+	//json 문법
+</script>
+
+<div>
+	<ul>
+		{#each todo as item, index}
+			<li>
+				{#if item.status === 0}
+					{item.content}
+					<button on:click={() => complete(index)}> complete </button>
+					<!-- button 여러개 each 안에 -->
+				{:else}
+					<strike>{item.content}</strike>
+				{/if}
+			</li>
+		{/each}
+	</ul>
+	<input bind:value />
+
+	<button on:click={() => handler()}> Add </button>
+</div>
+
+<style>
+    strike {
+        color : red;
+    }
+</style>
